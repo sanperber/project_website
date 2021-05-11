22b2d1bd38f025b8bf4666fd7bde7436ae69ec08
sanperber <santiago.perez@illuisvives.cat>
2021-05-11 21:04:42 +0200

    Adicción de un archivo JavaScript en la página web.

---
+++ javascript_website.js
@@ -0,0 +1,9 @@
+function miNumero(val) {
+    if (val == 15 || val == 51) {
+    return "Todo bien."
+    } else if (val == 0) {
+        return "Eing"
+    }
+    return "Todo mal."
+}
+miNumero(0);
